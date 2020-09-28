6dd1b2246d3b3cf9fceeca2c01815b650232364d
Aseem <aseemanand2@gmail.com>
2020-09-28 20:23:31 +0530

    [Aseem] Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeLadderPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder game with single player\n");
+            Console.ReadKey();
+        }
+
+    }
+}
