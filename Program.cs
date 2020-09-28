25d564e74f01cecd964b7c42eb6872d9ef03832e
Aseem <aseemanand2@gmail.com>
2020-09-28 20:35:18 +0530

    [Aseem] Added code to roll the die

--- Program.cs
+++ Program.cs
@@ -9,6 +9,12 @@ namespace SnakeLadderPractice
             Console.WriteLine("Snake and Ladder game with single player\n");
             int startPosition = 0;
             Console.WriteLine("Start Position= "+startPosition);
+
+            //Rolling of die to get a random number
+            Random rand = new Random();
+            int diceValue = rand.Next(1, 7);
+            Console.WriteLine("Dice value= "+diceValue);
+
             Console.ReadKey();
         }
 
