9fd02349e85a6fda3fbd96a8fda6686badf756f9
Aseem <aseemanand2@gmail.com>
2020-09-28 20:55:39 +0530

    [Aseem] Repeated till winning position is reached

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace SnakeLadderPractice
         {
             Console.WriteLine("Snake and Ladder game with single player\n");
             int startPosition = 0;
+            int currentPosition = 0;
             const int noPlay = 1;
             const int ladder = 2;
             const int snake = 3;
@@ -16,27 +17,38 @@ namespace SnakeLadderPractice
 
             //Rolling of die to get a random number
             Random rand = new Random();
-            int diceValue = rand.Next(1, 7);
-            Console.WriteLine("Dice value= "+diceValue);
+            
 
             //Options for the player (No play, Ladder, Snake)
-            int options = rand.Next(1, 4);
-            switch (options)
+            
+            while (currentPosition != 100)
             {
-                case noPlay:
-                    jump = 0;
-                    Console.WriteLine("No play: Jump= " + jump);
-                    break;
-                case ladder:
-                    jump = diceValue;
-                    Console.WriteLine("Ladder: Jump= " + jump);
-                    break;
-                case snake:
-                    jump = -diceValue;
-                    Console.WriteLine("Snake: Jump= " + jump);
-                    break;
+                int diceValue = rand.Next(1, 7);
+                int options = rand.Next(1, 4);
+                switch (options)
+                {
+                    case noPlay:
+                        jump = 0;
+                        Console.WriteLine("No play: Jump= " + jump);
+                        break;
+                    case ladder:
+                        jump = diceValue;
+                        Console.WriteLine("Ladder: Jump= " + jump);
+                        break;
+                    case snake:
+                        jump = -diceValue;
+                        Console.WriteLine("Snake: Jump= " + jump);
+                        break;
+                }
+                currentPosition += jump;
+                Console.WriteLine("Position after this die roll: " + currentPosition);
+                if (currentPosition < 0)
+                {
+                    currentPosition = startPosition;
+                    Console.WriteLine("Position reset to: " + currentPosition);
+                }
             }
-
+                
             Console.ReadKey();
         }
 
