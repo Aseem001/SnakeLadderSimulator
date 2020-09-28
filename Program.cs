28e447b36f7690a9f4912fc45ea8370a8c03f3ca
Aseem <aseemanand2@gmail.com>
2020-09-28 21:05:38 +0530

    [Aseem] Added dice count and position after every die roll

--- Program.cs
+++ Program.cs
@@ -6,24 +6,22 @@ namespace SnakeLadderPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Snake and Ladder game with single player\n");
+            Console.WriteLine("Snake and Ladder game\n");
             int startPosition = 0;
             int currentPosition = 0;
             const int noPlay = 1;
             const int ladder = 2;
             const int snake = 3;
+            int dieCount = 0;
             int jump = 0;
-            Console.WriteLine("Start Position= "+startPosition);
-
-            //Rolling of die to get a random number
             Random rand = new Random();
-            
-
-            //Options for the player (No play, Ladder, Snake)
-            
             while (currentPosition != 100)
             {
+                dieCount++;
+
+                Console.WriteLine("Dice count: " + dieCount);
                 int diceValue = rand.Next(1, 7);
+                Console.WriteLine("Dice value= " + diceValue);
                 int options = rand.Next(1, 4);
                 switch (options)
                 {
@@ -52,8 +50,9 @@ namespace SnakeLadderPractice
                     currentPosition -= jump;
                     Console.WriteLine("Position reset to: " + currentPosition);
                 }
+                Console.WriteLine();
+
             }
-                
             Console.ReadKey();
         }
 
